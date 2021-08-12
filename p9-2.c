f0b4825c93e6afe7b7b4e3567b7460b4dac6a3b3
IceHidden <xwh436016960@gmail.com>
2021-08-13 07:54:44 +0800

    Update workspace.xml

--- p9-2.c
+++ p9-2.c
@@ -1,9 +1,8 @@
 #include "stdio.h"
 
-int main()
-{
-    printf("整形输出%d\n",1+2.0);
-    printf("浮点型输出:%f\n",1+2.0);
+int main() {
+    printf("整形输出%d\n", 1 + 2.0);
+    printf("浮点型输出:%f\n", 1 + 2.0);
 
     return 0;
 }
