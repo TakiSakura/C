41dfc8c19c057202698f685e496fd13fff9b0951
IceHidden <xwh436016960@gmail.com>
2021-08-13 03:22:47 +0800

    /

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("调制方法!\n");
+    return 0;
+}
---
+++ p9-2.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main()
+{
+    printf("整形输出%d\n",1+2.0);
+    printf("浮点型输出:%f\n",1+2.0);
+
+    return 0;
+}
