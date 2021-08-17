c93596a7a6bbd30c34bdf398ace44af591e69f26
TakiSakura <xwh4366016960@gmail.com>
2021-08-17 09:15:39 +0800

    Merge remote-tracking branch 'origin/master'

--- for/p14-5.c
+++ for/p14-5.c
@@ -5,7 +5,7 @@ int main()
 {
     for (int i = 1; i < 9; ++i) {
         for (int j = 1; j <= i; ++j) {
-            printf("%d*%d=%d  ", i, j , i *j);
+            printf("%d*%d=%-2d  ", i, j , i *j);
         }
         putchar('\n');
     }
