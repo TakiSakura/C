ac0c57d18a4c47204f1aa574b36b8125f43d7211
TakiSakura <xwh4366016960@gmail.com>
2021-08-19 05:35:31 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ demo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    long int count = 1;
+
+    while(count <= 300000000) {
+
+        if(count % 100000000 == 0) {
+
+            printf("Multiple is %d\n", count / 100000000);
+        }
+
+        ++count;
+    }
+
+    return 0;
+}
